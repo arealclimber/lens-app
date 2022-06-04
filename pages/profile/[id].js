01bab33a85d22972d99b206953e94bb3d3358e37
Lumii <arealclimber@users.noreply.github.com>
2022-06-04 07:57:29 +0000

    [id].js dynamically render the id page

---
+++ pages/profile/[id].js
@@ -0,0 +1,16 @@
+import {useRouter} from 'next/router'
+
+export default function Profile() {
+    const router = useRouter()
+    const {id} = router.query
+
+
+    return (
+        <div>
+            {id}
+        </div>
+    )
+}
+
+
+
