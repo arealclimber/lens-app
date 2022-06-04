4f091569007e9b013fdf8af32b97628264800434
Lumii <arealclimber@users.noreply.github.com>
2022-06-04 08:59:40 +0000

    ongoing rendering image in [id].js

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,8 +1,26 @@
 import {useRouter} from 'next/router'
+import {useState, useEffect} from 'react'
+import {client, getProfiles} from '../../api'
+import Image from 'next/image'
 
 export default function Profile() {
     const router = useRouter()
     const {id} = router.query
+
+    useEffect(() => {
+        if (id) {
+            fetchProfile()
+        }
+    }, [id])
+
+    async function fetchProfile() {
+        try {
+            const response = await client.query(getProfiles, {id}).toPromise()
+            console.log('response: ', response)
+        } catch (err) {
+            console.error(err)
+        }
+    }
 
 
     return (
